34ffc2d987ddec9e622e2540ddd6fc80aa754983
Thiên Đỗ <yaibatamado@gmail.com>
2025-06-04 15:24:54 +0700

    Delete lab-01 directory

--- lab-01/ex02_01.py
+++
@@ -1,3 +0,0 @@
-ten = input("Nhập tên của bạn:  ")
-tuoi = input("Nhập tuổi của bạn:  ")
-print("Chào Mừng,", ten, "! Bạn", tuoi, "tuổi.")
--- lab-01/ex02_10.py
+++
@@ -1,4 +0,0 @@
-def dao_nguoc_chuoi(chuoi):
-    return chuoi[::-1]
-input_string = input("Mời nhập chuỗi cần đâor ngược: ")
-print("Chuỗi đảo ngược là: ", dao_nguoc_chuoi(input_string))
